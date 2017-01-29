82fad315f7e23bece0ca9e120c8e8c967a1c48e2
Raj Dhakate <rajdhakate@Rajs-Mac.local>
2017-01-29 21:18:32 +0530

    Initial Commit

---
+++ Calorie Meter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calorie Meter
+//
+//  Created by Raj Dhakate on 29/01/17.
+//  Copyright © 2017 Code Blood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calorie Meter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calorie Meter
+//
+//  Created by Raj Dhakate on 29/01/17.
+//  Copyright © 2017 Code Blood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
